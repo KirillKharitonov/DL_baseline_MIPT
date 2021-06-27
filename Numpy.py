5783033660880e894526f53813ce3641f6c1c5a0
Kirill K <k.kharitonov@tinkoff.ru>
2021-06-27 23:35:55 +0500

    added new decisions

--- Numpy.py
+++ Numpy.py
@@ -4,6 +4,8 @@ def matrixMult(first, second):
     """
        param first: list of "size" lists, each contains "size" floats
        param second: list of "size" lists, each contains "size" floats
+
+       :return: multiplication of first and second
     """
     # result = [first[i][j] * second[i][j] for i, j in zip(range(len(first)), len(range(second[0])))]
     result = [[0 for i in range(len(second[0]))] for j in range(len(first))]
@@ -14,11 +16,36 @@ def matrixMult(first, second):
     res2 = np.matmul(first, second)
     return result, res2
 
+def diag_2k(a):
+    """
+    :param a: np.array[size, size]
+    :return: Sum of even elements on main diagonal of a
+    """
+    result = np.sum([x for x in a.diagonal() if x % 2 == 0])
+
+    return result
+
+def cumsum(A):
+    """
+
+    :param A: np.array[m,n]
+    :return:
+    """
+    result = np.cumsum(A, axis = 1)
+
+    return result
+
+## tests
 a = [[1, 2, 3],
      [1, 2, 3],
      [1, 2, 3]]
 b = [[3, 2, 1],
      [4, 5, 6],
      [7, 8, 9]]
+A = [[3, 2, 1],
+     [4, 5, 6],
+     [7, 8, 9]]
 
-print(matrixMult(a, b))
+print(cumsum(A))
+# print(matrixMult(a, b))
+
